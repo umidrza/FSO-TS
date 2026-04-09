68928628e6d4c91a83422446615f19f89783c16b
umidrza <umidrza4@gmail.com>
2026-04-09 12:40:24 +0400

    add entry types

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -1,4 +1,8 @@
-import { z } from 'zod';
+import { z } from "zod";
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never;
 
 export type Diagnosis = {
   code: string;
@@ -6,31 +10,67 @@ export type Diagnosis = {
   latin?: string;
 };
 
-export type Entry = {
-  id: string;
-  date: string;
-  type: string;
-  specialist: string;
-  diagnosisCodes?: Array<Diagnosis['code']>;
-  description: string;
-};
-
-export type NewEntry = Omit<Entry, 'id'>;
-
-export const Gender = {
-  Male: 'male',
-  Female: 'female',
-  Other: 'other'
+export const HealthCheckRating = {
+  Healthy: 0,
+  LowRisk: 1,
+  HighRisk: 2,
+  CriticalRisk: 3,
 } as const;
 
-export type Gender = typeof Gender[keyof typeof Gender];
+export type HealthCheckRating =
+  (typeof HealthCheckRating)[keyof typeof HealthCheckRating];
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export type NewEntry = UnionOmit<Entry, "id">;
+
+export const Gender = {
+  Male: "male",
+  Female: "female",
+  Other: "other",
+} as const;
+
+export type Gender = (typeof Gender)[keyof typeof Gender];
 
 export const NewPatientSchema = z.object({
   name: z.string(),
   dateOfBirth: z.iso.date(),
   ssn: z.string(),
   gender: z.enum(Gender),
-  occupation: z.string()
+  occupation: z.string(),
 });
 
 export type NewPatient = z.infer<typeof NewPatientSchema>;
@@ -40,4 +80,4 @@ export interface Patient extends NewPatient {
   entries?: Entry[];
 }
 
-export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
