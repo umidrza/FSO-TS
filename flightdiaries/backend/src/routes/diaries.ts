3f2ee9d6dfa2abadfa6c06ff2e5c71310c4c7187
umidrza <umidrza4@gmail.com>
2026-04-09 11:44:45 +0400

    formating

--- flightdiaries/backend/src/routes/diaries.ts
+++ flightdiaries/backend/src/routes/diaries.ts
@@ -1,16 +1,20 @@
-import express, { type Request, type Response } from 'express';
-import diaryService from '../services/diaryService.ts';
-import { type DiaryEntry, type NewDiaryEntry, type NonSensitiveDiaryEntry } from '../types.ts';
-import { newDiaryParser, errorMiddleware } from '../middleware.ts';
+import express, { type Request, type Response } from "express";
+import diaryService from "../services/diaryService.ts";
+import {
+  type DiaryEntry,
+  type NewDiaryEntry,
+  type NonSensitiveDiaryEntry,
+} from "../types.ts";
+import { newDiaryParser, errorMiddleware } from "../middleware.ts";
 
 const router = express.Router();
 
-router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
+router.get("/", (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
   const data = diaryService.getNonSensitiveEntries();
   res.send(data);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const diary = diaryService.findById(Number(req.params.id));
 
   if (diary) {
@@ -20,10 +24,17 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', newDiaryParser, (req: Request<unknown, unknown, NewDiaryEntry>, res: Response<DiaryEntry>) => {  
-  const addedEntry = diaryService.addDiary(req.body);  
-  res.json(addedEntry);
-});
+router.post(
+  "/",
+  newDiaryParser,
+  (
+    req: Request<unknown, unknown, NewDiaryEntry>,
+    res: Response<DiaryEntry>,
+  ) => {
+    const addedEntry = diaryService.addDiary(req.body);
+    res.json(addedEntry);
+  },
+);
 
 router.use(errorMiddleware);
 
